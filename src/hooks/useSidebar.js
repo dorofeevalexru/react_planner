d7c7a7cef250d0f23f6fa6b9f63b1a44212c8389
Alexander Dorofeev <66268316+dorofeevalexru@users.noreply.github.com>
2025-09-15 19:58:42 +0300

    Update useSidebar.js

--- src/hooks/useSidebar.js
+++ src/hooks/useSidebar.js
@@ -2,7 +2,6 @@ import { useCallback } from 'react';
 
 export const useSidebar = () => {
   const initSidebar = useCallback(() => {
-    // Инициализация сайдбара будет здесь
     console.log('Sidebar initialized');
   }, []);
 
